34ea4002507116d7eed506d4f88df6cf2135cc91
Doaa-Atef <doaa.atef.hemed@gmail.com>
2025-05-01 22:51:21 +0300

    - splash_screen Animation - implement search screen

--- lib/shared/widgets/product_item.dart
+++ lib/shared/widgets/product_item.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../../../core/routes/routes.dart';
 import '../../../../core/styles/app_colors.dart';
 import '../../features/product_details/product_details_args.dart';
